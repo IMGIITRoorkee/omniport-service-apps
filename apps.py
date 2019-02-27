6db307ba6751da6cfa1addaec24d41430d4661ee
Dhruv Bhanushali <dhruv_b@live.com>
2019-02-28 01:47:58 +0530

    Rewrite the file to utilise the latest developments in the Omniport Configuration framework

--- apps.py
+++ apps.py
@@ -1,5 +1,3 @@
-from django.apps import AppConfig
+from configuration.utils.app_config_class import get_app_config_class
 
-
-class AppsConfig(AppConfig):
-    name = 'apps'
+Config = get_app_config_class(__file__)
